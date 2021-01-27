b3c9747bb2fabfd2ef695287a5268eb6c8aae8d3
Chauhan5devanshi <78071175+Chauhan5devanshi@users.noreply.github.com>
2021-01-27 12:55:21 +0530

    Add files via upload

---
+++ pp001.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	if(n%2)
+	{
+		printf("Number is odd");
+	}
+	else
+	{
+		printf("Number is even");
+	}
+}
