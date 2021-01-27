48a525e4696478fc1263945f607a626e7f3fddfa
Chauhan5devanshi <78071175+Chauhan5devanshi@users.noreply.github.com>
2021-01-27 12:58:56 +0530

    Update pp001.c

--- pp001.c
+++ pp001.c
@@ -1,9 +1,10 @@
+//Write a program to input a number and check it to be even or odd using if statement
 #include<stdio.h>
 main()
 {
 	int n;
 	printf("Enter the number : ");
-	scanf("%d",&n);
+	scanf("%d",&n);//input a number
 	if(n%2)
 	{
 		printf("Number is odd");
@@ -13,3 +14,6 @@ main()
 		printf("Number is even");
 	}
 }
+/*Output
+Enter the number:24
+Number is even*/
