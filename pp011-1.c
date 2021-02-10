b7f0c38092f8a63562a1f51422725d6c8eff1584
Chauhan5devanshi <78071175+Chauhan5devanshi@users.noreply.github.com>
2021-02-10 16:39:02 +0530

    Add files via upload

---
+++ pp011-1.c
@@ -0,0 +1,5 @@
+//is_even() function function checking a number is even and returns 1 else return 0. 
+int is_even(int n)
+{
+	return(n%2?0:1);
+}
---
+++ pp011-2.c
@@ -0,0 +1,13 @@
+//main() function file taking a number as a input.
+#include<stdio.h>
+main()
+{
+	int num;
+	printf("Enter the number :");
+	scanf("%d",&num);
+	if(is_even(num))printf("%d is even",num);
+	else printf("%d is odd",num);
+}
+/*OUTPUT
+Enter the number :2345
+2345 is odd*/
